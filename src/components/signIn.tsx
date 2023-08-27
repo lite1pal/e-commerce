f45ef590e31628c2464919a9f3ce995ce7822454
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2023-08-27 13:15:04 +0300

    removed border in signin

--- src/components/signIn.tsx
+++ src/components/signIn.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function SignIn() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center border bg-slate-900">
+    <div className="flex h-screen w-screen items-center justify-center bg-slate-900">
       <Link
         className="m-5 rounded-full bg-blue-700 px-5 py-3 font-mono text-white hover:animate-pulse"
         href="/api/auth/signin"
