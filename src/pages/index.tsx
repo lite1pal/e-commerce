1b8f4cb265dd6ae356b791cfbe3b702a708aa8c9
Denis Tarasenko <denistarasenko@Deniss-MacBook-Air.local>
2023-08-27 11:35:56 +0300

    on unathorization redirects to signIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,12 @@ import { redirect } from "next/navigation";
 import LoadingSpinner from "~/components/helpful/loading";
 
 export default function Home() {
-  const { data: sessionData, status } = useSession();
+  const { data: sessionData, status } = useSession({
+    required: true,
+    onUnauthenticated: () => {
+      redirect("/components/signIn");
+    },
+  });
 
   if (status === "loading" || !sessionData) {
     return <div className="">hello</div>;
